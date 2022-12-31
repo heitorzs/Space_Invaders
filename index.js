d1c5ab2a110400de5e43d30b058b00cc6e88f9ad
heitorzs <heitorzs@hotmail.com>
2022-12-31 11:55:53 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ class Player {
             x: 0,
             y: 0
         }
-       
+        this.rotation = 0
+
         const image = new Image()
         image.src = './Imagens/spaceship.png'
         image.onload = () => {
@@ -26,35 +27,180 @@ class Player {
             }
         }
     }
+
     draw() {
-        if (this.image){
-            c.drawImage(
-                this.image,
-                this.position.x,
-                this.position.y,
-                this.width,
-                this.height
-            )}
+        c.save()
+        c.translate(
+            player.position.x + player.width / 2,
+            player.position.y + player.height / 2
+        )
+        c.rotate(this.rotation)
+        c.translate(
+            -player.position.x - player.width / 2,
+            -player.position.y - player.height / 2
+        )
+
+        c.drawImage(
+            this.image,
+            this.position.x,
+            this.position.y,
+            this.width,
+            this.height
+        )
+        c.restore()
+    }
+    update() {
+        if (this.image) {
+            this.draw()
+            this.position.x += this.velocity.x
+        }
     }
 }
-const jogador1 = new Player()
+class Projectiles {
+    constructor({ position, velocity }) {
+        this.position = position
+        this.velocity = velocity
 
+        this.radius = 3
+    }
+    draw() {
+        c.beginPath()
+        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
+        c.fillStyle = 'white'
+        c.fill()
+        c.closePath()
+    }
+    update() {
+        this.draw()
+        this.position.x += this.velocity.x
+        this.position.y += this.velocity.y
+    }
+
+}
+
+class Invader {
+    constructor() {
+
+        this.velocity = {
+            x: 0,
+            y: 0
+        }
+
+        const image = new Image()
+        image.src = './Imagens/invader.png'
+        image.onload = () => {
+            const scale = 1;
+            this.image = image;
+            this.width = image.width * scale;
+            this.height = image.height * scale;
+            this.position = {
+                x: canvas.width / 2 - this.width / 2,
+                y: canvas.height 
+            }
+        }
+    }
+
+    draw() {
+      
+        c.drawImage(
+            this.image,
+            this.position.x,
+            this.position.y,
+            this.width,
+            this.height
+        )
+    }
+    update() {
+        if (this.image) {
+            this.draw()
+            this.position.x += this.velocity.x
+            this.position.y += this.velocity.y
+
+        }
+    }
+}
+
+const player = new Player()
+const projectiles = []
+const invader = new Invader()
+const keys = {
+    a: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+    space: {
+        pressed: false
+    }
+}
 function animate() {
     requestAnimationFrame(animate)
     c.fillStyle = 'black'
     c.fillRect(0, 0, canvas.width, canvas.height)
-    jogador1.draw()
+    invader.update()
+    player.update()
+    projectiles.forEach((projectile, index) => {
+        if (projectile.position.y + projectile.radius <= 0){
+            setTimeout(() => {
+                projectiles.splice(index, 1) 
+            }, 0)
+        } else {
+            projectile.update()
+        }
+    })
+    if (keys.a.pressed && player.position.x >= 0) {
+        player.velocity.x = -7
+        player.rotation = (- .25)
+    } else if (keys.d.pressed && player.position.x + player.width <= canvas.width) {
+        player.velocity.x = 7
+        player.rotation = .25
+    } else {
+        player.velocity.x = 0
+        player.rotation = 0
+    }
 }
 
 animate()
 
 addEventListener('keydown', ({ key }) => {
-    switch(key){
+    switch (key) {
         case "a":
             console.log("left")
+            keys.a.pressed = true
             break
         case "d":
             console.log("rigth")
+            keys.d.pressed = true
+            break
+        case " ":
+            console.log(projectiles)
+            keys.space.pressed = true
+            projectiles.push(
+                new Projectiles({
+                    position: {
+                        x: player.position.x + player.width / 2,
+                        y: player.position.y
+                    },
+                    velocity: {
+                        x: 0,
+                        y: -10
+                    }
+                })
+            )
+            break
+
+    }
+})
+addEventListener('keyup', ({ key }) => {
+    switch (key) {
+        case "a":
+            console.log("left")
+            keys.a.pressed = false
+            break
+        case "d":
+            console.log("rigth")
+            keys.d.pressed = false
             break
         case " ":
             console.log("space")
